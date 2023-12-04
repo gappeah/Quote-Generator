042ec9a730c206d392b832f9e342e54f32ab8ab7
George <appeahg@outlook.com>
2023-12-04 20:07:25 +0000

    building the front end

--- main.py
+++ main.py
@@ -2,13 +2,3 @@ import requests
 import tkinter as ttk
 import ttkbootstrap as ttk
 
-URL = "https://api.quotable.io/random"
-
-root = ttk.Window(themename="pulse")
-root.title("Quote Generator")
-root.geometry("700x250")
-
-frame = ttk.Frame(root)
-frame.pack(padx=30, pady=40)
-
-quote_label = ttk.Label(frame, text="Quote", font=("Helvetica", 16))
